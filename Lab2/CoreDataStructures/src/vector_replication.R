b86cf7a3790d78c0c0e6583c4634b943b01a2658
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-11-08 00:15:39 +0200

    create repeated vectors

---
+++ Lab2/CoreDataStructures/src/vector_replication.R
@@ -0,0 +1,11 @@
+# Create a vector 'k' by repeating the sequence 3 times
+k <- rep(c(8, 2, 6), times = 3)
+print(k)
+
+# Create a vector 'w' by repeating values individually
+w <- c(rep(4, 7), rep(9, 5), rep(2, 3))
+print(w)
+
+# Create a vector 'q' by repeating each value a specified number of times
+q <- rep(c(4, 9, 2), times = c(7, 5, 3))
+print(q)
